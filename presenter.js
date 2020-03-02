fdc0b8e7ebbe61b854156a7aae8f0ce954287a84
meisterk <franz@kohnlehome.de>
2020-03-02 13:51:15 +0100

    Add buttons

--- presenter.js
+++ presenter.js
@@ -2,6 +2,7 @@ import { View } from "./view.js";
 export class Presenter {
   constructor() {
     this.view = new View(this);
-    this.view.displayText("Hello");
+    this.view.renderQuestionView();
+    this.view.displayQuestion("Hello?");
   }
 }
--- view.js
+++ view.js
@@ -1,10 +1,26 @@
 export class View {
   constructor(presenter) {
     this.presenter = presenter;
-    this.app = document.getElementById("app");
   }
 
-  displayText(text) {
-    this.app.innerHTML = text;
+  renderQuestionView() {
+    this.app = document.getElementById("app");
+    this.pNode = document.createElement("p");
+    this.app.appendChild(this.pNode);
+    this.divNode = document.createElement("div");
+    this.app.appendChild(this.divNode);
+    const html = `<ul>
+    <li><button>One</button></li>
+    <li><button>Two</button></li>
+    <li><button>Three</button></li>
+    <li><button>Four</button></li>
+    <li><button>Five</button></li>
+    </ul>
+    `;
+    this.divNode.innerHTML = html;
+  }
+
+  displayQuestion(text) {
+    this.pNode.innerHTML = text;
   }
 }
